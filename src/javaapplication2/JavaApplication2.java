3f264cb0e82fa22b9f93bdf74123c9bec5324dd0
josteda99 <josteda99@VAIO>
2019-09-28 07:50:34 -0500

    helloda xD

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int c = 1;
+        for(int i=0;i<n;i++){
+            c = c * 2;
+            System.out.println(c);
+        }
+        
+    }
+}
